ad33ece55e46c63864d34fc911add4ec74980559
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2025-02-24 21:26:56 -0300

    Copy app bunoapp

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,179 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import {
+  Calendar,
+  ChevronUp,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+  User2,
+  UserPen,
+  CreditCard,
+  Sparkles,
+  BadgeCheck,
+  Bell,
+  LogOut,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import { logout } from "@/firebase/auth";
+
+// Menu items.
+const items = [
+  {
+    title: "Inicio",
+    url: "/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Buscar casos",
+    url: "/cases",
+    icon: Search,
+  },
+  {
+    title: "Mis casos",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Mi perfil",
+    url: "#",
+    icon: UserPen,
+  },
+  // {
+  //   title: "Calendario",
+  //   url: "#",
+  //   icon: Calendar,
+  // },
+  // {
+  //   title: "Ajustes",
+  //   url: "#",
+  //   icon: Settings,
+  // },
+];
+
+// Logged user
+const user = {
+  name: "shadcn",
+  email: "m@example.com",
+  avatar: "/avatars/shadcn.jpg",
+};
+
+export function AppSidebar() {
+  const router = useRouter();
+
+  async function handleLogout() {
+    try {
+      const response = await logout();
+      console.log(response);
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="ml-1">
+        <Image
+          src="/buno-coffee-bar-transparent-2.png"
+          alt="Logo"
+          width={140}
+          height={40}
+        />
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Plataforma</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton size="lg">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage src={user.avatar} alt={user.name} />
+                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">{user.name}</span>
+                    <span className="truncate text-xs">{user.email}</span>
+                  </div>
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <BadgeCheck className="w-4 h-4 mr-1.5" />
+                    Mi cuenta
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <CreditCard className="w-4 h-4 mr-1.5" />
+                    Facturación
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Bell className="w-4 h-4 mr-1.5" />
+                    Notificaciones
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleLogout}>
+                  <LogOut className="w-4 h-4 mr-1.5" />
+                  Cerrar sesión
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
