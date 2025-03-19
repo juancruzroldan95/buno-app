d4a7df07882744f9e6121d1f3009e66a0f792209
Juan Cruz Roldan <juancruz.roldan19@gmail.com>
2025-03-19 00:02:37 -0300

    Validate error first approach

--- src/components/ProfileAvatar.tsx
+++ src/components/ProfileAvatar.tsx
@@ -29,25 +29,24 @@ export default function ProfileAvatar({
 
   async function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
     const file = event.target.files?.[0];
-    if (file) {
-      try {
-        const url = await uploadFile(file);
-        setAvatarUrl(url);
-        await updateLawyer(lawyerId, {
-          profilePicture: url,
-        });
-        toast({
-          description: "La foto se guardó correctamente.",
-        });
-      } catch (error) {
-        console.error("HandleFileChange error:", error);
-        toast({
-          variant: "destructive",
-          title: "¡Oh no! Algo falló.",
-          description:
-            "No se pudo guardar la foto. Por favor, intentá de nuevo.",
-        });
-      }
+    if (!file) return;
+
+    try {
+      const url = await uploadFile(file);
+      setAvatarUrl(url);
+      await updateLawyer(lawyerId, {
+        profilePicture: url,
+      });
+      toast({
+        description: "La foto se guardó correctamente.",
+      });
+    } catch (error) {
+      console.error("HandleFileChange error:", error);
+      toast({
+        variant: "destructive",
+        title: "¡Oh no! Algo falló.",
+        description: "No se pudo guardar la foto. Por favor, intentá de nuevo.",
+      });
     }
   }
 
